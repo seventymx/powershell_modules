36f445c3557ecc1786c13ee7f765369041333b28
Steffen70 <steffen@seventy.mx>
2024-06-27 14:05:57 +0200

    Removed fallback values in Initialize-Fastlane function.

--- ShellUtils/ShellUtils.psm1
+++ ShellUtils/ShellUtils.psm1
@@ -62,12 +62,12 @@ function Set-AndroidEnvironment {
 
 function Initialize-Fastlane {
     param (
-        [string] $FastlaneAppIdentifier = $env:FASTLANE_APP_IDENTIFIER_FALLBACK,
-        [string] $FastlaneAppName = $env:FASTLANE_APP_NAME_FALLBACK,
-        [string] $FastlaneDebug = $env:FASTLANE_DEBUG_FALLBACK
+        [string] $FastlaneAppIdentifier,
+        [string] $FastlaneAppName,
+        [string] $FastlaneDebug
     )
 
-    # Set Fastlane environment variables with fallback values
+    # Set environment variables - App information
     $env:FASTLANE_APP_IDENTIFIER = $FastlaneAppIdentifier
     $env:FASTLANE_APP_NAME = $FastlaneAppName
     $env:FASTLANE_DEBUG = $FastlaneDebug
